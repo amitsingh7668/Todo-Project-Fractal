c9afd00b3e5cf041746d47f6e25b598f6699525d
amit singh <56405921+amitsingh7668@users.noreply.github.com>
2021-02-28 19:49:53 +0530

    Update todoTable.sql

--- socgen/sql/todoTable.sql
+++ socgen/sql/todoTable.sql
@@ -1,14 +1,7 @@
- CREATE TABLE Employee (  
-  userId  text,    
- employeefirstname text,  
-	  employeelastName text,
-	  password text,
-PRIMARY KEY (userId)  
-);  
-
-  userId  int,    
+ CREATE TABLE todo (  
+id  int,    
  task text,  
 	  status text,
 	  category text,
-PRIMARY KEY (userId)  
+PRIMARY KEY (id)  
 ); 
